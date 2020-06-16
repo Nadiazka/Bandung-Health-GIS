24e09d71523a2d42878958804db3143843bbef25
Nadiazka <nadiaazkahp@yahoo.com>
2020-06-16 11:11:11 +0700

    Update 16 Juni v6

--- BandungHealthGISv2/BandungHealthGISv2/settings.py
+++ BandungHealthGISv2/BandungHealthGISv2/settings.py
@@ -17,7 +17,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 TEMPLATE_DIR = os.path.join(BASE_DIR,"templates")
 STATIC_DIR = os.path.join(BASE_DIR,"static")
 #GDAL_LIBRARY_PATH='C:/Users/User/Miniconda3/Lib/site-packages/gdal-2.4.4/data'
-#GDAL_LIBRARY_PATH='C:/Users/User/Miniconda3/Lib/site-packages/django/contrib/gis/gdal/libgdal.py'
+GDAL_LIBRARY_PATH='C:/Users/User/Miniconda3/Lib/site-packages/django/contrib/gis/gdal/libgdal.py'
 
 
 # Quick-start development settings - unsuitable for production
